d83a36af8ac724f3a59a736abc1dd6f0f8d3cf6e
Tyler Jordan <tcjordan@wisc.edu>
2026-02-08 16:29:55 -0600

    Updated hyperparameters for colab

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -10,7 +10,7 @@ class SpoilerDataset(Dataset):
     Handles loading CSV and tokenizing text for BERT
     """
     
-    def __init__(self, csv_path, tokenizer: BertTokenizer, max_length=256):
+    def __init__(self, csv_path, tokenizer: BertTokenizer, max_length=512):
         """
         Initialize dataset
         
